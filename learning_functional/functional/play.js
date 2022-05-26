9487feb381a25d7fb496bff651350ae5a119eb9d
minukk <alsdnr4320@gmail.com>
2022-05-26 18:52:03 +0900

    Add: forEachObject, unless, print, times

--- learning_functional/functional/play.js
+++ learning_functional/functional/play.js
@@ -1,4 +1,38 @@
-import forEach from "../lib/es8-functional";
+import {
+  forEach,
+  forEachObject,
+  unless,
+  print,
+  times,
+} from "../lib/es8-functional";
 
 const array = [1, 2, 3];
 forEach(array, (data) => console.log(data));
+
+let tellType = (arg) => {
+  if (typeof arg === "function") arg();
+  else console.log("The passed data is " + arg);
+};
+
+let data = 1;
+let dataFn = () => {
+  console.log("Function!!!");
+};
+
+tellType(data);
+tellType(dataFn);
+
+let object = { a: 1, b: 2 };
+forEachObject(object, (k, v) => console.log(`${k}: ${v}`));
+
+forEach([1, 2, 3, 4, 5, 6, 7, 8], (num) => {
+  unless(num % 2, () => {
+    print(`${num} is even`);
+  });
+});
+
+times(100, (num) => {
+  unless(num % 2, () => {
+    print(`${num} is even`);
+  });
+});
--- learning_functional/lib/es8-functional.js
+++ learning_functional/lib/es8-functional.js
@@ -1,8 +1,26 @@
-const forEach = (array, fn) => {
+export const forEach = (array, fn) => {
   let i;
   for (i = 0; i < array.length; ++i) {
     fn(array[i]);
   }
 };
 
-export default forEach;
+export const forEachObject = (obj, fn) => {
+  for (const property in obj) {
+    if (obj.hasOwnProperty(property)) {
+      fn(property, obj[property]);
+    }
+  }
+};
+
+export const unless = (predicate, fn) => {
+  if (!predicate) fn();
+};
+
+export const print = (print) => {
+  console.log(print);
+};
+
+export const times = (times, fn) => {
+  for (let i = 0; i < times; ++i) fn(i);
+};
