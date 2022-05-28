65738215a1f8df1637cac81c364a5fdbb1d31671
minukk <alsdnr4320@gmail.com>
2022-05-28 14:39:49 +0900

    Add: concatAll, reduce, zip

--- functional_array/functional/play.js
+++ functional_array/functional/play.js
@@ -1,6 +1,8 @@
 import arrayUtils from "../lib/es8-functional";
 
-arrayUtils.print(arrayUtils.map([1, 2, 3], (x) => x * x));
+const { concatAll, print, map, filter, reduce, zip } = arrayUtils;
+
+// print(map([1, 2, 3], (x) => x * x));
 
 const apressBooks = [
   {
@@ -33,13 +35,113 @@ const apressBooks = [
   },
 ];
 
-arrayUtils.print(
-  arrayUtils.map(apressBooks, (book) => ({
-    title: book.title,
-    author: book.author,
-  }))
+const apressBooks2 = [
+  {
+    name: "beginners",
+    bookDetails: [
+      {
+        id: 111,
+        title: "C# 6.0",
+        author: "ANDREW TROELSEN",
+        rating: [4.7],
+        reviews: [{ good: 4, excellent: 12 }],
+      },
+      {
+        id: 222,
+        title: "Efficient Learning Machines",
+        author: "Rahul Khanna",
+        rating: [4.5],
+        reviews: [],
+      },
+    ],
+  },
+  {
+    name: "pro",
+    bookDetails: [
+      {
+        id: 333,
+        title: "Pro AugularJS",
+        author: "Adam Freeman",
+        rating: [4.0],
+        reviews: [],
+      },
+      {
+        id: 444,
+        title: "Pro ASP.NET",
+        author: "Adam Freeman",
+        rating: [4.2],
+        reviews: [{ good: 14, excellent: 12 }],
+      },
+    ],
+  },
+];
+
+const reviewDetails = [
+  {
+    id: 111,
+    reviews: [{ good: 4, excellent: 12 }],
+  },
+  {
+    id: 222,
+    reviews: [],
+  },
+  {
+    id: 333,
+    reviews: [],
+  },
+  {
+    id: 444,
+    reviews: [{ good: 14, excellent: 12 }],
+  },
+];
+
+// print(
+//   map(apressBooks, (book) => ({
+//     title: book.title,
+//     author: book.author,
+//   }))
+// );
+
+// print(filter(apressBooks, (book) => book.rating[0] > 4.5));
+
+let useless = [2, 5, 6, 1, 10];
+
+let result = 0;
+useless.forEach((value) => {
+  result = result + value;
+});
+print(result);
+print(reduce(useless, (acc, val) => acc + val));
+
+let bookDetails = concatAll(map(apressBooks2, (book) => book.bookDetails));
+
+const reduceBookDetails = reduce(
+  bookDetails,
+  (acc, bookDetail) => {
+    let goodReviews =
+      bookDetail.reviews[0] !== undefined ? bookDetail.reviews[0].good : 0;
+    let excellentReviews =
+      bookDetail.reviews[0] !== undefined ? bookDetail.reviews[0].excellent : 0;
+
+    return {
+      good: acc.good + goodReviews,
+      excellent: acc.excellent + excellentReviews,
+    };
+  },
+  { good: 0, excellent: 0 }
 );
 
-arrayUtils.print(
-  arrayUtils.filter(apressBooks, (book) => book.rating[0] > 4.5)
-);
+print(reduceBookDetails);
+
+print(zip([1, 2, 3], [4, 5, 6], (a, b) => a + b));
+
+let mergedBookDetails = zip(bookDetails, reviewDetails, (book, review) => {
+  if (book.id === review.id) {
+    let clone = Object.assign({}, book);
+    clone.ratings = review;
+
+    return clone;
+  }
+});
+
+print(mergedBookDetails);
--- functional_array/lib/es8-functional.js
+++ functional_array/lib/es8-functional.js
@@ -9,6 +9,10 @@ const map = (array, fn) => {
 
   for (const value of array) result.push(fn(value));
 
+  // for (let i = 0; i < array.length; ++i) {
+  //   result.push(fn(array[i]));
+  // }
+
   return result;
 };
 
@@ -20,10 +24,63 @@ const filter = (array, fn) => {
   return result;
 };
 
+const concatAll = (array, fn) => {
+  let results = [];
+
+  for (const value of array) {
+    results.push.apply(results, value);
+    console.log(results);
+  }
+  // for (let i = 0; i < array.length; ++i) {
+  //   results.push.apply(results, array[i]);
+  // }
+
+  return results;
+};
+
+// const reduce = (array, fn) => {
+//   let acc = 0;
+
+//   for (const value of array) acc = fn(acc, value);
+
+//   return [acc];
+// };
+
+const reduce = (array, fn, init) => {
+  let acc;
+
+  if (init !== undefined) acc = init;
+  else acc = array[0];
+
+  if (init === undefined) {
+    for (let i = 1; i < array.length; ++i) {
+      acc = fn(acc, array[i]);
+    }
+  } else {
+    for (const value of array) {
+      acc = fn(acc, value);
+    }
+  }
+
+  return [acc];
+};
+
+const zip = (leftArr, rightArr, fn) => {
+  let results = [];
+
+  for (let i = 0; i < Math.min(leftArr.length, rightArr.length); ++i)
+    results.push(fn(leftArr[i], rightArr[i]));
+
+  return results;
+};
+
 const arrayUtils = {
   print: print,
   map: map,
   filter: filter,
+  concatAll: concatAll,
+  reduce: reduce,
+  zip: zip,
 };
 
 export default arrayUtils;
